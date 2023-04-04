05f4255f44c1f4c0164a5b6f54597be7d9d04475
martin.nagy <xraycow@gmail.com>
2023-04-04 11:25:01 +0200

    MapExercise1

---
+++ src/main/java/com/github/martinnagy/javaoptimizationexercise/MapExercise1.java
@@ -0,0 +1,60 @@
+package com.github.martinnagy.javaoptimizationexercise;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.StopWatch;
+
+public class MapExercise1 {
+    public static void main(String[] args) {
+        Random random = new Random(0);
+        List<Name> names = IntStream.range(0, 5_000_000)
+                .mapToObj(i -> new Name(randomLongString(random), randomLongString(random)))
+                .toList();
+
+        StopWatch stopWatch = StopWatch.createStarted();
+        optimizeMe(names);
+        System.out.println(stopWatch);
+    }
+
+    private static void optimizeMe(List<Name> names) {
+        Map<Name, String> cache = new HashMap<>();
+
+        for (Name name : names) {
+            if (cache.containsKey(name)) {
+                System.out.println(cache.get(name));
+            } else {
+                String concat = name.getFirstName() + " " + name.getLastName();
+                cache.put(name, concat);
+                System.out.println(concat);
+            }
+        }
+    }
+
+    private static String randomLongString(Random random) {
+        return StringUtils.repeat(random.nextInt(100) + "", 100);
+    }
+
+    private static class Name {
+        private final String firstName;
+        private final String lastName;
+
+        public Name(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+    }
+
+}
